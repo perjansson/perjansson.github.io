d4194df4f9e57e40451ff686ec48da4e2edd9d05
Per Jansson <per.r.jansson@gmail.com>
2020-10-26 22:30:26 +0200

    Add Lighthouse assertions

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,6 +4,12 @@ module.exports = {
       staticDistDir: './build',
       url: ['http://localhost:5000'],
     },
+    assert: {
+      assertions: {
+        'categories:performance': ['warn', { minScore: 1 }],
+        'categories:accessibility': ['error', { minScore: 1 }],
+      },
+    },
     upload: {
       target: 'temporary-public-storage',
     },
